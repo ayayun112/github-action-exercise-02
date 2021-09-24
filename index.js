b204032ece84b9c33a6c694bb5f874374a724217
Areeya Yunprayong <61160132@go.buu.ac.th>
2021-09-24 14:14:20 +0700

    test

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const port = process.env.port || 8080
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!')
+  res.send('Hello world!!!')
+})
+
+app.get('/users', (req, res) => {
+  res.send('No user in the system')
 })
 
 app.listen(port, () => {
-    console.log('The app is up and running!')
+  console.log('The app is up and running!')
 })
+
 module.exports = app
